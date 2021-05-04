07b147446033359d791cc46e07e7a645444368b7
erdiertas <ertaserdi@gmail.com>
2021-05-04 11:28:06 +0300

    fixed

--- lib/controller/ConnectController.php
+++ lib/controller/ConnectController.php
@@ -56,6 +56,33 @@ class ConnectController extends Controller
         return [];
     }
 
+    public function prompt_silent($prompt = "Enter Password:")
+    {
+        if (preg_match('/^win/i', PHP_OS)) {
+            $vbscript = sys_get_temp_dir() . 'prompt_password.vbs';
+            file_put_contents(
+                $vbscript, 'wscript.echo(InputBox("'
+                . addslashes($prompt)
+                . '", "", "password here"))');
+            $command = "cscript //nologo " . escapeshellarg($vbscript);
+            $password = rtrim(shell_exec($command));
+            unlink($vbscript);
+            return $password;
+        } else {
+            $command = "/usr/bin/env bash -c 'echo OK'";
+            if (rtrim(shell_exec($command)) !== 'OK') {
+                trigger_error("Can't invoke bash");
+                return;
+            }
+            $command = "/usr/bin/env bash -c 'read -s -p \""
+                . addslashes($prompt)
+                . "\" mypassword && echo \$mypassword'";
+            $password = rtrim(shell_exec($command));
+            echo "\n";
+            return $password;
+        }
+    }
+
 
     public function actionIndex($params)
     {
@@ -72,9 +99,9 @@ class ConnectController extends Controller
             $username = fgets($handle);
             fclose($handle);
             echo "Password: ";
-            system('stty -echo');
-            $password = trim(fgets(STDIN));
-            system('stty echo');
+//            system('stty -echo');
+            $password = $this->prompt_silent();
+//            system('stty echo');
             echo "\n\n";
             $login = Curl::post("login", [
                 'username' => $username,
